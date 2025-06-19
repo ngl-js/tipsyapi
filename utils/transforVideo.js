3a2bab236a3db849ea2d8d1bb6c29997b5c8ef42
ngl-js <joshua.dq@outlook.com>
2025-06-19 05:48:38 -0500

    Custom audio start

--- utils/transforVideo.js
+++ utils/transforVideo.js
@@ -1,25 +1,28 @@
 import Ffmpeg from "fluent-ffmpeg";
 
-export const mergeVideo= async (file_path, audio_file) => {
-  let audio_path = './assets/audio/'+audio_file;
-  let out_path = './upload/'+Date.now() + '.mp4';
+export const mergeVideo = async (file_path, audioStrObj) => {
+  console.log(audioStrObj);
+  let audio = JSON.parse(audioStrObj);
+  let audio_path = "./assets/audio/" + audio.name;
+  let out_path = "./upload/" + Date.now() + ".mp4";
 
-  const makevideo= new Promise((resolve)=> {
+  const makevideo = new Promise((resolve) => {
     let newMp4 = Ffmpeg();
     newMp4
       .input(file_path)
       .addInput(audio_path)
-      .size('50%')
+      .seekInput(audio.start)
+      .size("50%")
       .duration(15)
       .save(out_path)
-      .on('error', (err, stdout, stderr) => {
-        console.log('An error occurred: ' + err.message, err, stderr);
+      .on("error", (err, stdout, stderr) => {
+        console.log("An error occurred: " + err.message, err, stderr);
       })
-      .on('end', () => {
+      .on("end", () => {
         resolve(out_path);
       });
-  })
+  });
 
-  const out_video= await makevideo.then(data=> (data));
+  const out_video = await makevideo.then((data) => data);
   return out_video;
-}
+};
